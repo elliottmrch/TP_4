6b94046c29f89c430049785c812846efe95d128c
Elliott Amrouche <elliottamrouche27@gmail.com>
2024-10-21 11:05:10 +0200

    Exercice 2

--- TP_4/main.c
+++ TP_4/main.c
@@ -18,6 +18,75 @@ void InitGrille(int grille[9][9], int n) {
 	}
 }
 
+void ModifGrille(int grille[9][9], int n) {
+	char continuer;
+	int stop = 0;
+	int ligne = 0;
+	int colonne = 0;
+	int nbr = 0;
+
+	while (stop == 0) {
+		printf("Voulez vous modifier la grille ? (O = Oui / N = Non) : ");
+		scanf_s("%c", &continuer);
+		getchar();
+		if (continuer == 'O') {
+			stop = 0;
+		}
+		else {
+			stop = 1;
+			break;
+		}
+
+		printf("Numero de la LIGNE a modifier : ");
+		scanf_s("%d", &ligne);
+		getchar();
+		while (ligne < 0 || ligne >= n) {
+			if (ligne < 0 || ligne>= n) {
+				printf("Numero hors de la grille !\nNumero de la LIGNE a modifier : ");
+				scanf_s("%d", &ligne);
+				getchar();
+			}
+		}
+		
+
+		printf("Numero de la COLONNE a modifier : ");
+		scanf_s("%d", &colonne);
+		getchar();
+		while (colonne < 0 || colonne >= n) {
+			if (colonne < 0 || colonne >= n) {
+				printf("Numero hors de la grille !\nNumero de la COLONNE a modifier : ");
+				scanf_s("%d", &colonne);
+				getchar();
+			}
+		}
+		
+
+		printf("Numero a modifier : ");
+		scanf_s("%d", &nbr);
+		getchar();
+		while (nbr < 0 || nbr > 9) {
+			if (nbr < 0 || nbr > 9) {
+				printf("Numero non valide !\nNumero a modifier : ");
+				scanf_s("%d", &nbr);
+				getchar();
+			}
+		}
+		
+
+		grille[ligne][colonne] = nbr;
+
+		for (int x = 0; x < n; x++)
+		{
+			for (int y = 0; y < n; y++)
+			{
+				printf("%d ", grille[x][y]);
+			}
+			printf("\n");
+		}
+	}
+	
+}
+
 int main()
 {
 	int grille_facile[9][9] = {
@@ -45,6 +114,7 @@ int main()
 	};
 
 	InitGrille(grille_moyenne, 8);
+	ModifGrille(grille_moyenne, 8);
 
 	return 0;
 }
